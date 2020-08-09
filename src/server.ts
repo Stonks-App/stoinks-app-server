04932099df0589617f8af3195043684f50bfc5cd
chiragthesia <harithesia@gmail.com>
2020-08-09 17:57:53 -0500

    fixed docker files

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 import * as Sentry from '@sentry/node';
 import cors from 'cors';
+const port = process.env.PORT || 4000;
 
 Sentry.init({ dsn: 'https://50d76930b7304c4c94d6162db957ac8b@o425237.ingest.sentry.io/5364716' });
 
@@ -19,8 +20,12 @@ const resolvers = {
 	}
 };
 
-export const app = express();
-export const server = new ApolloServer({ typeDefs: typeDefs, resolvers: resolvers });
+const app = express();
+const server = new ApolloServer({ typeDefs: typeDefs, resolvers: resolvers });
 
 server.applyMiddleware({ app });
 app.use(cors());
+
+app.listen(port, () => {
+	console.log(`🚀Server is ready at http://localhost:${port}${server.graphqlPath}`);
+});

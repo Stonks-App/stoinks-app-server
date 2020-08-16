a821add7ee8b59b2e76fa5597b089379510f8601
chiragthesia <harithesia@gmail.com>
2020-08-15 20:08:29 -0500

    First push

--- src/gqlModules/dataSources/AlphaVantageAPI.ts
+++ src/gqlModules/dataSources/AlphaVantageAPI.ts
@@ -4,22 +4,21 @@ import { Data } from 'algotrader';
 import DataLoader from 'dataloader';
 
 export class AlphaVantageAPI extends RESTDataSource {
+	private api: any;
 
-    private api: any;
-    
-    constructor() {
-        super();
-        const AlphaVantage = Data.AlphaVantage;
-        this.api = new AlphaVantage("I73R9VKBL7Z5J4KM");
-    }
-    
-    private dailyTimeSeriesLoader = new DataLoader(async (stockSymbols: readonly string[]) => {
-        return stockSymbols.map( async (symbol) => {
-            return await this.api.timeSeriesDaily(symbol, true, true);
-        });
-    });
-  
-    async getDailyTimeSeries(stockSymbol: string) {
-      return this.dailyTimeSeriesLoader.load(stockSymbol);
-    }
+	constructor() {
+		super();
+		const AlphaVantage = Data.AlphaVantage;
+		this.api = new AlphaVantage('I73R9VKBL7Z5J4KM');
+	}
+	//@ts-ignore
+	private dailyTimeSeriesLoader = new DataLoader(async (stockSymbols: string[]) => {
+		return stockSymbols.map(async (symbol) => {
+			return await this.api.timeSeriesDaily(symbol, true, true);
+		});
+	});
+
+	async getDailyTimeSeries(stockSymbol: string) {
+		return this.dailyTimeSeriesLoader.load(stockSymbol);
+	}
 }

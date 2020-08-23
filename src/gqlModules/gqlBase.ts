90fd50429fb501fd667fb0470a812520ce41295a
Anish V <a.vaghasia@utexas.edu>
2020-08-23 00:37:15 -0500

    Fix resolver merging

--- src/gqlModules/gqlBase.ts
+++ src/gqlModules/gqlBase.ts
@@ -1,8 +1,22 @@
+import merge from 'lodash.merge';
+
 import { defaultTypeDefs } from './shared/defaults/defaultTypeDefs';
-import { typeDefs as exampleTypeDefs, resolvers as exampleResolvers } from './exampleModule/exampleModule';
-import { typeDefs as dailyTypeDefs, resolvers as dailyResolvers } from './dailyModule/dailyModule';
-import { typeDefs as discordTypeDefs, resolvers as discordResolvers } from './discordModule/discordModule';
-import { typeDefs as optionTypeDefs, resolvers as optionResolvers } from './optionsModule/optionsModule';
+import {
+  typeDefs as exampleTypeDefs,
+  resolvers as exampleResolvers
+} from './exampleModule/exampleModule';
+import {
+  typeDefs as dailyTypeDefs,
+  resolvers as dailyResolvers
+} from './dailyModule/dailyModule';
+import {
+  typeDefs as discordTypeDefs,
+  resolvers as discordResolvers
+} from './discordModule/discordModule';
+import {
+  typeDefs as optionTypeDefs,
+  resolvers as optionResolvers
+} from './optionsModule/optionsModule';
 import { AlphaVantageAPI } from './shared/dataSources/AlphaVantageAPI';
 import { DiscordAPI } from './shared/dataSources/DiscordAPI';
 import { RobinhoodAPI } from './shared/dataSources/RobinhoodAPI';
@@ -17,30 +31,30 @@ const baseTypeDef = `
 `;
 
 export const typeDefs = [
-	baseTypeDef,
-	defaultTypeDefs,
-	exampleTypeDefs,
-	dailyTypeDefs,
-	discordTypeDefs,
-	optionTypeDefs
+  baseTypeDef,
+  defaultTypeDefs,
+  exampleTypeDefs,
+  dailyTypeDefs,
+  discordTypeDefs,
+  optionTypeDefs
 ];
 
 const baseResolver = {
-	Query: {}
+  Query: {}
 };
 
-export const resolvers = {
-	...baseResolver,
-	...exampleResolvers,
-	...dailyResolvers,
-	...discordResolvers,
-	...optionResolvers
-};
+export const resolvers = merge(
+  baseResolver,
+  exampleResolvers,
+  dailyResolvers,
+  discordResolvers,
+  optionResolvers
+);
 
 export const dataSources = () => {
-	return {
-		alphaVantageAPI: new AlphaVantageAPI(),
-		discordAPI: new DiscordAPI(),
-		robinhoodAPI: new RobinhoodAPI()
-	};
+  return {
+    alphaVantageAPI: new AlphaVantageAPI(),
+    discordAPI: new DiscordAPI(),
+    robinhoodAPI: new RobinhoodAPI()
+  };
 };
--- src/gqlModules/optionsModule/optionsModule.ts
+++ src/gqlModules/optionsModule/optionsModule.ts
@@ -1,39 +1,59 @@
 export const typeDefs = `
-    extend type Query {
-        getOptionInfo(stockSymbol: String): OptionInstrument
-    }
-    type OptionInstrument {
-        meta: String
-    }
+extend type Query {
+  optionInstrument(stockSymbol: String, expirationDate: String, strikePrice: String): OptionInstrument
+}
+type OptionInstrument {
+  meta: OptionMeta,
+  price: OptionPrice,
+  volume: OptionVolume,
+  interest: OptionInterest,
+  greeks: Greeks
+}
+type OptionMeta {
+  stockSymbol: String,
+  expirationDate: String,
+  strikePrice: String,
+  robinHoodUrl: String
+}
+type OptionPrice {
+  ask: String,
+  bid: String,
+  mark: String,
+  volatility: String,
+}
+type OptionVolume {
+  ask: String,
+  bid: String,
+  mark: String
+}
+type OptionInterest {
+  openInterest: Int,
+  highFillBuyPrice: String,
+  highFillSellPrice: String
+}
+type Greeks {
+  delta: String,
+  gamma: String,
+  theta: String
+}
 `;
 
 export const resolvers = {
-	Query: {
-		getOptionInfo: async (
-			_source: any,
-			{
-				stockSymbol,
-				expirationDate,
-				strikePrice
-			}: { stockSymbol: string; expirationDate: string; strikePrice: string },
-			//@ts-ignore
-			{ dataSources: { RobinhoodAPI } }
-		) => {
-			console.log(stockSymbol);
-			console.log(expirationDate);
-			console.log(strikePrice);
-			return {
-				meta: 'hello'
-			};
-		}
-	}
+  Query: {
+    optionInstrument: async (
+      _source: any,
+      {
+        stockSymbol,
+        expirationDate,
+        strikePrice
+      }: { stockSymbol: string; expirationDate: string; strikePrice: string },
+      //@ts-ignore
+      { dataSources: { RobinhoodAPI } }
+    ) => {
+      console.log(stockSymbol);
+      console.log(expirationDate);
+      console.log(strikePrice);
+      return {};
+    }
+  }
 };
-
-// _source: any,
-// {
-//   stockSymbol,
-//   expirationDate,
-//   strikePrice
-// }: { stockSymbol: String; expirationDate: String; strikePrice: String },
-// //@ts-ignore
-// { dataSources: { RobinhoodAPI } }

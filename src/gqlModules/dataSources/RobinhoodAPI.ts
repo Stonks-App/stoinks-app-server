86f6f1d36307db335df38c9fe2645300c0b58b35
Anish V <a.vaghasia@utexas.edu>
2020-08-22 20:04:25 -0500

    Get authentication working

--- src/gqlModules/dataSources/RobinhoodAPI.ts
+++ src/gqlModules/dataSources/RobinhoodAPI.ts
@@ -1,37 +1,50 @@
 import { RESTDataSource } from 'apollo-datasource-rest';
-const { User } = require('algotrader').Robinhood;
+// @ts-ignore
+import { Robinhood } from 'algotrader';
+const User = Robinhood.User;
 
 const options = {
-	doNotSaveToDisk: false, // If the `save` method should not store the user login info to disk (file)
-	serializedUserFile: null // File to where the serialized user login info can be saved
+	doNotSaveToDisk: true
 };
-export class RobinhoodAPI extends RESTDataSource {
-	// private user: any;
 
+let mainUser: any = null
+
+export class RobinhoodAPI extends RESTDataSource {
 	constructor() {
 		super();
-		console.log('hitting this? ');
-		const user = new User('stonks.go.brrr@gmail.com', options);
-		console.log(user);
-		user
-			.authenticate()
-			.then(() => {
-				console.log('auth');
+		this.getUser();
+	}
+
+	private getUser() {
+		const loadedUser = mainUser;
+		if (loadedUser) {
+			console.log('Load saved user')
+			if (!loadedUser.isAuthenticated()) {
+				console.log('authenticate saved user');
+				loadedUser.reauthenticate();
+			}
+			return loadedUser;
+		} else {
+			console.log('Create new user')
+			const user = new User('stonks.go.brrr@gmail.com', 'qLS&fUmIM3c&2*j^Qx3', 'e0063428-35ad-4f61-974c-c181ba33482c', options);
+			return this.authenticateUser(user);
+		}
+	}
+
+	private authenticateUser(user: any) {
+		return user
+			.authenticate('qLS&fUmIM3c&2*j^Qx3')
+			.then((authenticatedUser: any) => {
+				console.log('RobinHood user authenticated');
+				// authenticatedUser.save();
+				mainUser = authenticatedUser;
+				return authenticatedUser;
 			})
 			.catch((e: any) => {
 				console.log(e);
+				return null;
 			});
 	}
-
-	// isAuthenticate() {
-	// 	this.user
-	// 		.authenticate()
-	// 		.then(() => {
-	// 			console.log('authenticated');
-	// 			this.getPortfolio();
-	// 		})
-	// 		.catch((error: any) => console.log(error));
-	// }
 
 	// async getPortfolio() {
 	// 	const data = await this.user.getPortfolio();

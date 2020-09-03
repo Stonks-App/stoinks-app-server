e1d7d8b5b1defb947b86b5bf746988ec120e8f10
Anish V <a.vaghasia@utexas.edu>
2020-09-02 22:55:30 -0500

    Add discord message caching

--- src/gqlModules/shared/dataSources/DiscordAPI.ts
+++ src/gqlModules/shared/dataSources/DiscordAPI.ts
@@ -1,5 +1,27 @@
 import { RESTDataSource } from 'apollo-datasource-rest';
+import axios from 'axios';
 import { parseMessage, parseTrade } from './utils/messageParser';
+import { DiscordMessage } from './utils/types';
+
+interface MessageDBInterface {
+  [key: string]: [DiscordMessage];
+}
+
+const messageDB: MessageDBInterface = {};
+
+const discordAxios = axios.create({
+  baseURL: 'https://discord.com/api',
+  timeout: 1000,
+  headers: { Authorization: process.env.DISCORD_AUTH_TOKEN }
+});
+
+export const discordMessagePull = (channelID: string) => {
+  discordAxios
+    .get<[DiscordMessage]>(`/channels/${channelID}/messages?limit=100`)
+    .then(({ data }: { data: [DiscordMessage] }) => {
+      messageDB[channelID] = data;
+    });
+};
 
 export class DiscordAPI extends RESTDataSource {
   constructor() {
@@ -13,6 +35,12 @@ export class DiscordAPI extends RESTDataSource {
 
   //const nexsus-swing-channel = 694323672483364874
   async getDiscordMessages(channelID: number, numMessages: number) {
+    const messagesFromDB = messageDB[channelID];
+    if (messagesFromDB) {
+      console.log('FETCHED FROM CACHE');
+      return messagesFromDB.splice(0, numMessages).map(parseMessage);
+    }
+    console.log('NOT FETCHED FROM CACHE');
     const messages = await this.get(
       `/channels/${channelID}/messages?limit=${numMessages}`
     );
--- src/server.ts
+++ src/server.ts
@@ -7,28 +7,34 @@ const port = process.env.PORT || 4000;
 import logger from './utils/log/logger';
 
 import { typeDefs, resolvers, dataSources } from './gqlModules/gqlBase';
+import { startDiscordJob } from './jobs/jobs';
 
 Sentry.init({
-	dsn: 'https://50d76930b7304c4c94d6162db957ac8b@o425237.ingest.sentry.io/5364716'
+  dsn:
+    'https://50d76930b7304c4c94d6162db957ac8b@o425237.ingest.sentry.io/5364716'
 });
 
 const schema = makeExecutableSchema({
-	typeDefs,
-	// @ts-ignore
-	resolvers
+  typeDefs,
+  // @ts-ignore
+  resolvers
 });
 
 const app = express();
 const server = new ApolloServer({
-	schema,
-	dataSources,
-	playground: true,
-	introspection: true
+  schema,
+  dataSources,
+  playground: true,
+  introspection: true
 });
 
 server.applyMiddleware({ app });
 app.use(cors());
 
+startDiscordJob();
+
 app.listen(port, () => {
-	logger.info(`🚀Server is ready at http://localhost:${port}${server.graphqlPath}`);
+  logger.info(
+    `🚀Server is ready at http://localhost:${port}${server.graphqlPath}`
+  );
 });

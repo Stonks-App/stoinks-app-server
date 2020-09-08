de5f1e5d02d05e45ea170271f2684a8cda566794
chiragthesia <harithesia@gmail.com>
2020-09-07 23:58:47 -0500

    Updated mongo connection

--- src/db/mongoConnection.ts
+++ src/db/mongoConnection.ts
@@ -3,17 +3,21 @@ import mongoose from 'mongoose';
 export interface db {}
 
 export default (db: string) => {
-	const connect = () => {
-		mongoose
-			.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-			.then(() => {
-				return console.log(`Successfully connected to ${db}`);
-			})
-			.catch((error) => {
-				console.log('Error connecting to database: ', error);
-				return process.exit(1);
-			});
-	};
-	connect();
-	mongoose.connection.on('disconnected', connect);
+	try {
+		const connect = () => {
+			mongoose
+				.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+				.then(() => {
+					return console.log(`💾 Successfully connected to ${db}`);
+				})
+				.catch((error) => {
+					console.log('Error connecting to database: ', error);
+					return process.exit(1);
+				});
+		};
+		connect();
+		mongoose.connection.on('disconnected', connect);
+	} catch (error) {
+		console.log(error);
+	}
 };
